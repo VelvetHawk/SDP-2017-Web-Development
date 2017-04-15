d3e1d53c796b593052a47fd4dd728b034fcacfed
Brian Dooley <brian_dooley9@yahoo.ie>
2017-04-15 22:49:56 +0100

    Users who sign up must now verify there account by email. Users who forgot their password can now change there password via email link. Banned users are now unable to login and are greeted with a popup upon attemtping to login

---
+++ activate.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BD
+ * Date: 15-Apr-17
+ * Time: 6:32 PM
+ */
---
+++ password_recovery_link.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bd
+ * Date: 14/04/2017
+ * Time: 20:23
+ */
